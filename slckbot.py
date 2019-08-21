62b9b3f021604e53a77c2e9269ec03eb24b9fda4
Yeonggyun.Kim <kygha7205@naver.com>
2019-08-21 17:26:38 +0900

    Add escape condition for user input of files
    Signed-off-by: Yeonggyun.Kim <kygha7205@naver.com>

--- slckbot.py
+++ slckbot.py
@@ -21,49 +21,27 @@ def run():
         try:
             msg = json.loads(ws.recv())
             print(msg)
+            if 'files' in msg.keys():
+                print("사진입력!! 스킵~")
+                continue
             if 'text' in msg.keys() and 'user' in msg.keys():
                 # slack.chat.post_message(msg['channel'], msg['text'] + "의 날씨정보를 가져오고 있습니다. 잠시만 기다려 주세요..")
                 result = shorty.wordpop(msg['text'])
                 fin = ''.join(result)
                 slack.chat.post_message(msg['channel'], fin)
-                # break
-                # return
+
         except websocket.WebSocketTimeoutException:
             ws.send(json.dumps({'type': 'ping'}))
 
         except websocket.WebSocketConnectionClosedException:
             print("Connection closed")
-            # break
+
             return
         except Exception as e:
             print("error : ", e)
-            # break
+
             return
 
 
 # while True:
 run()
-
-
-# async def execute_bot():
-#     ws = await websockets.connect(sock_endpoint)
-#     while True:
-#         message_json = json.loads(await ws.recv())
-#         # print(message_json['type'])
-#         if 'content' in message_json:
-#             input_message = message_json['content']
-#             print(input_message)
-#             slack.chat.post_message(message_json['channel'],input_message)
-#             continue
-#         else:
-#             print(message_json)
-#
-#
-# loop = asyncio.new_event_loop()
-# asyncio.set_event_loop(loop)
-# asyncio.get_event_loop().run_until_complete(execute_bot())
-# asyncio.get_event_loop().run_forever()
-
-
-def slack_notify(text=None, channel='random', username="Weatherpidia", attachments=None):
-    slack.chat.post_message(text=text, channel=channel, username=username, attachments=attachments)
